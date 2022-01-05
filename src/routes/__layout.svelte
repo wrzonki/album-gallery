dfeddd643d09b37ef3a9bf1a9bec53ae6b60d4d9
Pawel Wrzosek <wrzonki@gmail.com>
2022-01-05 23:52:10 +0100

    added styles, refactored code 🎁👑

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import '../styles/global.css'
+</script>
+
+<header><h1>Album gallery</h1></header>
+<slot/>
+
+<style>
+  h1 {
+    text-align: center;
+    font-size: 900;
+    font-style: italic;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,35 @@
+<script context="module">
+  const baseUrl = 'https://jsonplaceholder.typicode.com/albums';
+
+  export const load = async ({ fetch }) => {
+    const res = await fetch(baseUrl);
+    const albums = await res.json();
+
+    return {
+      props: {
+        albums
+      }
+    }
+  }
+</script>
+
+<script>
+  import AlbumCard from '$lib/AlbumCard.svelte';
+  export let albums;
+</script>
+
+<div class="wrapper">
+  {#each albums as album}
+    <AlbumCard {...album}/>
+  {/each}
+</div>
+
+<style>
+  .wrapper {
+    display: grid;
+    grid-template-columns: repeat( auto-fit, minmax(250px, 1fr) );
+    gap: 20px;
+    padding: 20px;
+  }
+</style>
+

ecb3da07e2c3c1bbc0b0f48897df26beb7abc2c2
Pawel Wrzosek <wrzonki@gmail.com>
2022-01-06 00:19:06 +0100

    added footer o(*￣▽￣*)o

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,6 +6,11 @@
   <a href="/"><h1>Album gallery</h1></a>
 </header>
 <slot/>
+<footer>
+  <a href="https://github.com/wrzonki/album-gallery">
+    <img src="/gh.png" alt="git-hub logo"/>
+  </a>
+</footer>
 
 <style>
   h1 {
@@ -17,5 +22,19 @@
   a {
     text-decoration: none;
     color: #000;
+  }
+
+  footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    text-align: center;
+    background-color: #fff;
+    box-shadow: 0 0 1px var(--grey);
+  }
+
+  footer img {
+    width: 100px;
   }
 </style>

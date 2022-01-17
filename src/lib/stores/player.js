28d6b87eec288ba2d0fb7363d5f5ea5e122d24a1
Pawel Wrzosek <wrzonki@gmail.com>
2022-01-17 23:22:11 +0100

    added

--- src/lib/stores/player.js
+++ src/lib/stores/player.js
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 
 export const audioPlayer = writable({
-  id: 237,
-  name: 'song 1',
-  url: 'https://pawel-wrzosek.pl/stock-audio/1.mp3',
+  id: null,
+  name: '',
+  url: '',
 });
